ea00743172c97f39deec66ad0c4f748d0b2ac6f7
Berry <berryflezzyfrosh@gmail.com>
2024-10-28 03:42:22 -1200

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed3 = new typed3(".text", {
+    Strings: ["Frontend Developer", "YouTuber" , "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
